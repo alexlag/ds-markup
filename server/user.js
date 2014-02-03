b9f2929d5e55cd21e4404cb151d32cf97e63e67f
alexlag <bo4onok@gmail.com>
2014-02-03 12:38:28 +0400

    Another validation fix

--- server/user.js
+++ server/user.js
@@ -1,12 +1,24 @@
 Accounts.validateNewUser(function (user) {
 	var profiles = _.pluck(Meteor.users.find().fetch() || [], 'profile');
-	var numbers = _.pluck(profiles, 'number') || [];
-	if(_.contains(numbers, user.profile.number))
-		throw new Meteor.Error(403, "Student number was already registered");
+	if(profiles && profiles.length > 0) {
+		var numbers = _.map(profiles, function(el) {
+			if(el && el.number)
+				return el.number;
+			else
+				return '' 
+		});
+		if(_.contains(numbers, user.profile.number))
+			throw new Meteor.Error(403, "Student number was already registered");
 
-	var fullnames = _.pluck(profiles, 'fullname');
-	if(_.contains(fullnames, user.profile.fullname))
-		throw new Meteor.Error(403, "Full name was already registered");
+		var fullnames = _.map(profiles, function(el) {
+			if(el && el.fullname)
+				return el.fullname;
+			else
+				return '' 
+		});
+		if(_.contains(fullnames, user.profile.fullname))
+			throw new Meteor.Error(403, "Full name was already registered");
+	}
 
 	return true;
 });
