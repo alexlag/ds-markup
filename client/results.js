8124988b3761632d8798360d1314107917f77e8a
alexlag <bo4onok@gmail.com>
2014-02-18 20:03:32 +0400

    result page cleanup 2

--- client/results.js
+++ client/results.js
@@ -1,14 +1,17 @@
 Template.resultsTable.entries = function() {
-	var result = Session.get('resultsArr') || [{
-		name: '',
-		cssClass: '',
-		w: '',
-		d1: '',
-		d2: '',
-		d3: '',
-		score: ''
-	}];
-	return result;
+	var result = Session.get('resultsArr');
+	if(result && result.length !== 0)
+		return result;
+	else
+		return [{
+			name: '',
+			cssClass: '',
+			w: '',
+			d1: '',
+			d2: '',
+			d3: '',
+			score: ''
+		}];
 }
 
 Template.resultsTable.rendered = function() {
