9f11c34cc7dd0023bd018ca8b36b322420d97e8b
alexlag <bo4onok@gmail.com>
2014-02-20 21:14:09 +0400

    rollback

--- models/submission.js
+++ models/submission.js
@@ -1,37 +1,3 @@
-CollectionFS.prototype.fileIsAllowed = function(fileRecord) {
-            var self = this;
-            if (!self._filter) {
-                return true;
-            }
-            if (!fileRecord || fileRecord.contentType === null || !fileRecord.filename) {
-                throw new Error("invalid fileRecord:", fileRecord);
-            }
-            var fileSize = fileRecord.size || parseInt(fileRecord.length, 10);
-            if (!fileSize || isNaN(fileSize)) {
-                throw new Error("invalid fileRecord file size:", fileRecord);
-            }
-            var filter = self._filter;
-            if (filter.maxSize && fileSize > filter.maxSize) {
-                self.dispatch('invalid', CFSErrorType.maxFileSizeExceeded, fileRecord);
-                return false;
-            }
-            var saveAllFileExtensions = (filter.allow.extensions.length === 0);
-            var saveAllContentTypes = (filter.allow.contentTypes.length === 0);
-            var ext = getFileExtension(fileRecord.filename);
-            var contentType = fileRecord.contentType;
-            if (!((saveAllFileExtensions || _.indexOf(filter.allow.extensions, ext) !== -1) &&
-                    _.indexOf(filter.deny.extensions, ext) === -1)) {
-                self.dispatch('invalid', CFSErrorType.disallowedExtension, fileRecord);
-                return false;
-            }
-            if (!((saveAllContentTypes || contentTypeInList(filter.allow.contentTypes, contentType)) &&
-                    !contentTypeInList(filter.deny.contentTypes, contentType))) {
-                self.dispatch('invalid', CFSErrorType.disallowedContentType, fileRecord);
-                return false;
-            }
-            return true;
-        }
-
 SubmissionsFS = new CollectionFS('submissions', { autopublish: false });
 
 SubmissionsFS.filter({
